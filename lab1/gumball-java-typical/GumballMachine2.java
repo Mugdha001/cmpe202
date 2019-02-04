1c138bd4419b0d9719121311cdb7376e195cf19f
Mugdha Wadikar <mugdhawadikar@gmail.com>
2019-02-04 13:54:13 -0800

    Different class approach for all 3 gumball classes

--- lab1/gumball-java-typical/GumballMachine2.java
+++ lab1/gumball-java-typical/GumballMachine2.java
@@ -1,20 +1,26 @@
 
-public class GumballMachine
+/**
+ * Write a description of class GumballMachine2 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class GumballMachine2 extends Main
 {
-
+    
     private int num_gumballs;
     private boolean has_quarter;
 
-    public GumballMachine( int size )
+    public GumballMachine2( int size )
     {
         // initialise instance variables
         this.num_gumballs = size;
         this.has_quarter = false;
     }
 
-    public void insertQuarter(int coin)
+    public void insertQuarter(int coin1, int coin2)
     {
-        if ( coin == 25 )
+        if ( coin1 == 25 && coin2 == 25)
             this.has_quarter = true ;
         else 
             this.has_quarter = false ;
@@ -28,16 +34,16 @@ public class GumballMachine
     		{
     			this.num_gumballs-- ;
     			this.has_quarter = false ;
-    			System.out.println( "Thanks for your quarter.  Gumball Ejected!" ) ;
+    			System.out.println( "Thanks for your 2 quarters.  Gumball Ejected!" ) ;
     		}
     		else
     		{
-    			System.out.println( "No More Gumballs!  Sorry, can't return your quarter." ) ;
+    			System.out.println( "No More Gumballs!  Sorry, can't return your coin/s." ) ;
     		}
     	}
     	else 
     	{
-    		System.out.println( "Please insert a quarter" ) ;
+    		System.out.println( "Please insert 2 coins of a quarter each" ) ;
     	}        
     }
 }
--- lab1/gumball-java-typical/Main.java
+++ lab1/gumball-java-typical/Main.java
@@ -2,21 +2,30 @@
 
 public class Main {
 
-	public static void main(String[] args) {
-		GumballMachine gumballMachine = new GumballMachine(5);
+    public static void main(String[] args) {
+        GumballMachine1 gumballMachine1 = new GumballMachine1(6);
+        GumballMachine2 gumballMachine2 = new GumballMachine2(5);
+        GumballMachine3 gumballMachine3 = new GumballMachine3(7);
+        //System.out.println(gumballMachine);
 
-		System.out.println(gumballMachine);
+        gumballMachine1.insertQuarter( 25 );
+        gumballMachine1.turnCrank();
+        gumballMachine2.insertQuarter( 25 , 25);
+        gumballMachine2.turnCrank();
+        int []arr = new int[3];
+        arr[0]=10;
+        arr[1]=10;
+        arr[2]=30;
+        gumballMachine3.insertQuarter(arr);
+        gumballMachine3.turnCrank();
 
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
+        //System.out.println(gumballMachine);
 
-		System.out.println(gumballMachine);
+        //gumballMachine1.insertQuarter( 15 );
+        //gumballMachine1.turnCrank();
+        //gumballMachine.insertQuarter( 35 );
+        //gumballMachine.turnCrank();
 
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-
-		System.out.println(gumballMachine);
+		//System.out.println(gumballMachine);
 	}
 }
